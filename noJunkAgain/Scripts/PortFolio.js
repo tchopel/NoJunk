1cc4ccce8ccfcc9831c2a37bf71a328049b03f6c
chopel tsering <chopeltsering@gmail.com>
2014-08-02 12:34:48 -0400

    fixed dimensioning after ajaxing

--- noJunkAgain/Scripts/PortFolio.js
+++ noJunkAgain/Scripts/PortFolio.js
@@ -1,38 +1,74 @@
 ﻿/// <reference path="../Views/Home/_projectBell.cshtml" />
 
 $(function () {
-    
+
+    fixDimensions();
+
+    loadPortfolio();
+
+
+});
+
+
+function loadPortfolio() {
+    $('#leftNav span').click(function () {
+        var url = "@Url.Action(\"projectRobotic\", \"Home\", null, Request.Url.Scheme);"
+        $("#centerColumn").load("/Home/projectBell", function (responseText, statusText, xhr) {
+            if (statusText == "success")
+                fixDimensions();
+            if (statusText == "error")
+                alert("An error occurred: " + xhr.status + " - " + xhr.statusText);
+        });
+        //setTimeout(function () { fixDimensions() }, 100);
+    });
+
+
+    $('#rightNav span').click(function () {
+        var url = "@Url.Action(\"projectRobotic\", \"Home\", null, Request.Url.Scheme);"
+        $("#centerColumn").load("/Home/projectBell", function (responseText, statusText, xhr) {
+            if (statusText == "success")
+                fixDimensions();
+            if (statusText == "error")
+                alert("An error occurred: " + xhr.status + " - " + xhr.statusText);
+        });
+        //setTimeout(function () { fixDimensions() }, 100);
+    });
+}
+
+function fixDimensions() {
+
     var tabContainerId = $('#bootStrapContainer').parent().attr('id');
     var absolutedContainerHeight = $('#bootStrapContainer').height();
     var marginOfAbsolutedContainerHeight = $('#bootStrapContainer').css('margin-top');
     var totalHeight = parseFloat(absolutedContainerHeight) + parseFloat(marginOfAbsolutedContainerHeight);
     var heightOfTabContainer = $('#' + tabContainerId).height();
 
-    console.log("tabContainerHeight : " + heightOfTabContainer);
+    //console.log("tabContainerHeight : " + heightOfTabContainer);
 
-    if(heightOfTabContainer<totalHeight)
-        $('#'+tabContainerId).height(totalHeight);
+    if (heightOfTabContainer < totalHeight)
+        $('#' + tabContainerId).height(totalHeight);
 
     $('#leftNav span, #rightNav span').css({
         position: "relative",
-        top: parseFloat(absolutedContainerHeight)/2 + "px"
+        top: parseFloat(absolutedContainerHeight) / 2 + "px"
     });
-    
-    console.log("tabcontainerId : " + tabContainerId);
-    console.log("absolutedContainerHeight : " + absolutedContainerHeight);
-    console.log("marginOfABsolutedContainerHeight : " + marginOfAbsolutedContainerHeight);
-    console.log("totalHeight : " + totalHeight);
-    console.log("span top : " + parseFloat(marginOfAbsolutedContainerHeight) / 2);
 
-    $('#leftNav span, #rightNav span').click(function () {
-        var url = "@Url.Action(\"projectRobotic\", \"Home\", null, Request.Url.Scheme);"
-        $("#centerColumn").load("/Home/projectBell", function (responseText, statusText, xhr) {
-            if (statusText == "success")
-                alert("Successfully loaded the content!");
-            if (statusText == "error")
-                alert("An error occurred: " + xhr.status + " - " + xhr.statusText);
-        });
-    });
+    //console.log("tabcontainerId : " + tabContainerId);
+    //console.log("absolutedContainerHeight : " + absolutedContainerHeight);
+    //console.log("marginOfABsolutedContainerHeight : " + marginOfAbsolutedContainerHeight);
+    //console.log("totalHeight : " + totalHeight);
+    //console.log("span top : " + parseFloat(marginOfAbsolutedContainerHeight) / 2);
+}
+ 
+function loadSlideDiv(id) {
+    $.ajax({
+        url: "~/Views/Shared/projectRobot.cshtml",
+        cache: true
+    })
+  .done(function (html) {
+      $(id).append(html);
+  });
+}
 
     //$(document).keydown(function (e) {
     //    switch (e.which) {
@@ -55,15 +91,4 @@ $(function () {
     //        default: return; // exit this handler for other keys
     //    }
     //    e.preventDefault(); // prevent the default action (scroll / move caret)
-    //});
-});
-
-function loadSlideDiv(id) {
-    $.ajax({
-        url: "~/Views/Shared/projectRobot.cshtml",
-        cache: true
-    })
-  .done(function (html) {
-      $(id).append(html);
-  });
-}
+//});
