9da6bc0825099022b4d0339f58a5c3f8e5eca0e1
chopel tsering <chopeltsering@gmail.com>
2014-08-10 01:31:21 -0400

    commiting before visual studio restarts my computer

--- noJunkAgain/Scripts/projectRobotic.js
+++ noJunkAgain/Scripts/projectRobotic.js
@@ -1,8 +1,6 @@
-﻿
-
-$('#roboticImgContainer img').click(function () {
+﻿$('#roboticImgContainer img').click(function () {
     $img = $(this);
-    $('#roboticImgContainer').parent().css('z-index', -1);
+    $('#findParent').parent().css('z-index', -1);
     $('<img />', {
         'src': $img.attr('src'),
         'class': 'RoboticLarger',
@@ -11,7 +9,7 @@ $('#roboticImgContainer img').click(function () {
         $(this)
             .appendTo('body')
             .position({
-                "of": $('#roboticVidContainer'),
+                "of": $('#roboticImgContainer'),
                 "my": "center center",
                 "at": "center center",
                 "collision": "fit"
@@ -24,7 +22,7 @@ $('#roboticImgContainer img').click(function () {
                 'easing': 'easeOutQuad',
                 'step': function (i) {
                     $(this).position({
-                        "of": $('#roboticVidContainer'),
+                        "of": $('#roboticImgContainer'),
                         "my": "center center",
                         "at": "center center",
                         "collision": "fit"
@@ -37,7 +35,7 @@ $('#roboticImgContainer img').click(function () {
 $('body').on('click', '.RoboticLarger', function () {
     $('.RoboticLarger').fadeOut('slow', function () {
         $('.RoboticLarger').remove();
-        $('#roboticImgContainer').parent().css('z-index', 1);
+        $('#findParent').parent().css('z-index', 1);
     });
 });
 
@@ -49,94 +47,3 @@ $('video').on('seeked', function () {
     this.playbackRate = 3.0;
     alert('seeked');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//$(window).load(function () {
-//    function left(element, using) {
-//        element.position({
-//            "my": "right middle",
-//            "at": "left+45 middle",
-//            "of": $("#roboticSecondRow"),
-//            "collision": "none",
-//            using: using
-//        });
-//    }
-
-//    function right(element, using) {
-//        element.position({
-//            'my': "left middle",
-//            'at': "right-45 middle",
-//            'of': $("#roboticSecondRow"),
-//            'collision': "none",
-//            'using': using
-//        });
-//    }
-//    function center(element, using) {
-//        element.position({
-//            'my': "center middle",
-//            'at': "center middle",
-//            'of': $("#roboticSecondRow"),
-//            'using': using
-//        });
-//    }
-//    left($("#roboticContainer img:eq(0)"));
-//    center($("#roboticContainer img:eq(1)"));
-//    right($("#roboticContainer img:eq(2)"));
-
-//    function animate(to) {
-//        $(this).animate(to);
-//        console.log(to);
-//    }
-//    function next(event) {
-//        event.preventDefault();
-//        center($("#roboticContainer img:eq(2)"), animate);
-//        left($("#roboticContainer img:eq(1)"), animate);
-//        right($("#roboticContainer img:eq(0)").appendTo("#roboticSecondRow"));
-//    }
-//    function previous(event) {
-//        event.preventDefault();
-//        center($("#roboticContainer img:eq(0)"), animate);
-//        right($("#roboticContainer img:eq(1)"), animate);
-//        left($("#roboticContainer img:eq(2)").prependTo("#roboticSecondRow"));
-//    }
-//    $("#previous").click(previous);
-//    $("#next").click(next);
-
-//    $("#roboticContainer img").click(function (event) {
-//        $("#roboticContainer img").index(this) === 0 ? previous(event) : next(event);
-//    });
-
-//    $(window).resize(function () {
-//        left($("#roboticContainer img:eq(0)"), animate);
-//        center($("#roboticContainer img:eq(1)"), animate);
-//        right($("#roboticContainer img:eq(2)"), animate);
-//    });
-//});
